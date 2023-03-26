c6a32915759279121faac9171e721351614095d3
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2023-03-26 11:05:07 +0200

    #02 26/03/2023
    move route to Postcontroller.store and improve on

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,11 +1,43 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Application from "@ioc:Adonis/Core/Application";
+import * as fs from "fs";
 
 export default class PostsController {
   public async index({}: HttpContextContract) {}
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({request, response}: HttpContextContract) {
+    // console.log({
+    //   body: request.body(),
+    //   file: request.file('file',{
+    //     size: '5mb',
+    //     extnames: ['jpg','png','gif']
+    //   })
+    // })
+    const postInfo = request.body()
+    const postFile = request.file('file', {
+      size: '5mb',
+      extnames: ['jpg','png','gif']
+    })
+    if (!postFile) return
+    if (!postFile.isValid) return postInfo.errors
+    const date = new Date()
+    const year = date.getFullYear()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    // get folder content length
+    const dir = `./resources/assets/post/${year}/${month}/${day}`
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    const files = fs.readdirSync(`resources/assets/post/${year}/${month}/${day}`)
+    await postFile.move(Application.makePath(`resources/assets/post/${year}/${month}/${day}`), {
+      name: `${files.length + 1}.${postFile.extname}`,
+    })
+
+    return response
+  }
 
   public async show({}: HttpContextContract) {}
 
--- start/routes.ts
+++ start/routes.ts
@@ -19,31 +19,9 @@
 */
 
 import Route from "@ioc:Adonis/Core/Route";
-import Application from '@ioc:Adonis/Core/Application'
 
 Route.get("/", async ({ inertia }) => {
   return inertia.render("Home");
 });
 
-Route.post('/postimg', async ({ request, response}) => {
-  console.log({
-    body: request.body(),
-    file: request.file('file',{
-      size: '5mb',
-      extnames: ['jpg','png','gif']
-    })
-  })
-  const postInfo = request.body()
-  const postFile = request.file('file', {
-    size: '5mb',
-    extnames: ['jpg','png','gif']
-  })
-  if (!postFile) return
-  if (!postFile.isValid) return postInfo.errors
-
-  // Voir ce que fais le .move et voir si le path ./ est a la racine de C:
-  await postFile.move(Application.tmpPath('uploads'))
-  await postFile.moveToDisk('./')
-
-  return response
-})
+Route.post('/postimg', 'PostsController.store')
