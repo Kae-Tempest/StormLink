663a25c8ad6c8cb20f158d8e080f102da4096d59
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2023-06-21 17:42:41 +0200

    #15.1 21/06/2023

--- app/Validators/UserValidator.ts
+++ app/Validators/UserValidator.ts
@@ -5,6 +5,10 @@ export default class UserValidator {
   constructor(protected ctx: HttpContextContract) {}
   public schema = schema.create({
     username: schema.string({}, [rules.maxLength(12)]),
+    email: schema.string({ trim: true }, [
+      rules.email(),
+      rules.unique({ table: "users", column: "email", caseInsensitive: true }),
+    ]),
     password: schema.string({}, [rules.minLength(8)]),
   });
   public messages: CustomMessages = {
